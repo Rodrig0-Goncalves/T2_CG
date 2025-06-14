81ac9d170684cba686f6944506d5f489c9dd4fcc
Rodrigo Gonçalves <161329845+Rodrig0-Goncalves@users.noreply.github.com>
2025-06-14 16:16:17 -0300

    Criado movimento frente e tras + queda
    Iniciado botões de vídeo

--- Objeto3D.py
+++ Objeto3D.py
@@ -1,156 +1,227 @@
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 from OpenGL.GL import *
-from Ponto import *
+import time
+from Objeto3D import *
 
-import random
+o:Objeto3D
+tempo_antes = time.time()
+soma_dt = 0
+movimentos = "não"
 
-class Objeto3D:
-        
-    def __init__(self):
-        self.vertices = []
-        self.faces    = []
-        self.speed    = []
-        self.angle    = []
-        self.radius   = []
-        self.position = Ponto(0,0,0)
-        self.rotation = (0,0,0,0)
-        self.fase = 0
-        self.amplitude = 0.05
-        self.z_original = []
-        
-        pass
+estado  = "rodando"
+estados = []
 
-    def LoadFile(self, file:str):
-        f = open(file, "r")
+def init():
+    global o
+    glClearColor(0.5, 0.5, 0.9, 1.0)
+    glClearDepth(1.0)
 
-        # leitor de .obj baseado na descrição em https://en.wikipedia.org/wiki/Wavefront_.obj_file    
-        for line in f:
-            values = line.split(' ')
-            # dividimos a linha por ' ' e usamos o primeiro elemento para saber que tipo de item temos
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_CULL_FACE)
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
-            if values[0] == 'v': 
-                # item é um vértice, os outros elementos da linha são a posição dele
-                self.vertices.append(Ponto(float(values[1]),
-                                           float(values[2]),
-                                           float(values[3])))
-                self.speed.append((random.random() + 0.1))
-                
+    o = Objeto3D()
+    o.LoadFile('Human_Head.obj')
 
-                self.angle.append(math.atan2(float(values[3]), float(values[1])))
-                self.radius.append(math.hypot(float(values[1]), float(values[3])))
+    DefineLuz()
+    PosicUser()
 
 
-            if values[0] == 'f':
-                # item é uma face, os outros elementos da linha são dados sobre os vértices dela
-                self.faces.append([])
-                for fVertex in values[1:]:
-                    fInfo = fVertex.split('/')
-                    # dividimos cada elemento por '/'
-                    self.faces[-1].append(int(fInfo[0]) - 1) # primeiro elemento é índice do vértice da face
-                    # ignoramos textura e normal
-                
-        # calcular o centro da cabeça
-        soma_x, soma_y, soma_z = 0, 0, 0
-        contador = 0
-        for v in self.vertices:
-            if 0.4 < v.y < 0.6:
-                soma_x += v.x
-                soma_y += v.y
-                soma_z += v.z
-                contador += 1
-                
-        self.centro = Ponto(soma_x/ contador, soma_y/contador, soma_z/contador)
-        pass
+def DefineLuz():
+    # Define cores para um objeto dourado
+    luz_ambiente = [0.4, 0.4, 0.4]
+    luz_difusa = [0.7, 0.7, 0.7]
+    luz_especular = [0.9, 0.9, 0.9]
+    posicao_luz = [2.0, 3.0, 0.0]  # PosiÃ§Ã£o da Luz
+    especularidade = [1.0, 1.0, 1.0]
 
-    def DesenhaVertices(self):
+    # ****************  Fonte de Luz 0
+
+    glEnable(GL_COLOR_MATERIAL)
+
+    #Habilita o uso de iluminaÃ§Ã£o
+    glEnable(GL_LIGHTING)
+
+    #Ativa o uso da luz ambiente
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, luz_ambiente)
+    # Define os parametros da luz nÃºmero Zero
+    glLightfv(GL_LIGHT0, GL_AMBIENT, luz_ambiente)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, luz_difusa)
+    glLightfv(GL_LIGHT0, GL_SPECULAR, luz_especular)
+    glLightfv(GL_LIGHT0, GL_POSITION, posicao_luz)
+    glEnable(GL_LIGHT0)
+
+    # Ativa o "Color Tracking"
+    glEnable(GL_COLOR_MATERIAL)
+
+    # Define a reflectancia do material
+    glMaterialfv(GL_FRONT, GL_SPECULAR, especularidade)
+
+    # Define a concentraÃ§Ã£oo do brilho.
+    # Quanto maior o valor do Segundo parametro, mais
+    # concentrado serÃ¡ o brilho. (Valores vÃ¡lidos: de 0 a 128)
+    glMateriali(GL_FRONT, GL_SHININESS, 51)
+
+def PosicUser():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+    # Configura a matriz da projeção perspectiva (FOV, proporção da tela, distância do mínimo antes do clipping, distância máxima antes do clipping
+    # https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/gluPerspective.xml
+    gluPerspective(60, 16/9, 0.01, 50)  # Projecao perspectiva
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+    #Especifica a matriz de transformação da visualização
+    # As três primeiras variáveis especificam a posição do observador nos eixos x, y e z
+    # As três próximas especificam o ponto de foco nos eixos x, y e z
+    # As três últimas especificam o vetor up
+    # https://registry.khronos.org/OpenGL-Refpages/gl2.1/xhtml/gluLookAt.xml
+    gluLookAt(-2, 6, -8, 0, 0, 0, 0, 1.0, 0)
+
+def DesenhaLadrilho():
+    glColor3f(0.5, 0.5, 0.5)  # desenha QUAD preenchido
+    glBegin(GL_QUADS)
+    glNormal3f(0, 1, 0)
+    glVertex3f(-0.5, 0.0, -0.5)
+    glVertex3f(-0.5, 0.0, 0.5)
+    glVertex3f(0.5, 0.0, 0.5)
+    glVertex3f(0.5, 0.0, -0.5)
+    glEnd()
+
+    glColor3f(1, 1, 1)  # desenha a borda da QUAD
+    glBegin(GL_LINE_STRIP)
+    glNormal3f(0, 1, 0)
+    glVertex3f(-0.5, 0.0, -0.5)
+    glVertex3f(-0.5, 0.0, 0.5)
+    glVertex3f(0.5, 0.0, 0.5)
+    glVertex3f(0.5, 0.0, -0.5)
+    glEnd()
+
+def DesenhaPiso():
+    glPushMatrix()
+    glTranslated(-20, -1, -10)
+    for x in range(-20, 20):
         glPushMatrix()
-        glTranslatef(self.position.x, self.position.y, self.position.z)
-        glRotatef(self.rotation[3], self.rotation[0], self.rotation[1], self.rotation[2])
-        glColor3f(.0, .0, .0)
-        glPointSize(8)
-
-        # glBegin(GL_POINTS)
-        for v in self.vertices:
-            glPushMatrix()
-            glTranslate(v.x, v.y, v.z)
-            glutSolidSphere(.05, 20, 20)
-            glPopMatrix()
-            # glVertex(v.x, v.y, v.z)
-        # glEnd()
-        
+        for z in range(-20, 20):
+            DesenhaLadrilho()
+            glTranslated(0, 0, 1)
         glPopMatrix()
-        pass
+        glTranslated(1, 0, 0)
+    glPopMatrix()
 
-    def DesenhaWireframe(self):
-        glPushMatrix()
-        glTranslatef(self.position.x, self.position.y, self.position.z)
-        glRotatef(self.rotation[3], self.rotation[0], self.rotation[1], self.rotation[2])
-        glColor3f(0, 0, 0)
-        glLineWidth(2)        
+def DesenhaCubo():
+    glPushMatrix()
+    glColor3f(1, 0, 0)
+    glTranslated(0, 0.5, 0)
+    glutSolidCube(1)
+
+    glColor3f(0.5, 0.5, 0)
+    glTranslated(0, 0.5, 0)
+    glRotatef(90, -1, 0, 0)
+    glRotatef(45, 0, 0, 1)
+    glutSolidCone(1, 1, 4, 4)
+    glPopMatrix()
+
+def Animacao():
+    global soma_dt, tempo_antes, movimentos
+
+    tempo_agora = time.time()
+    delta_time = tempo_agora - tempo_antes
+    tempo_antes = tempo_agora
+
+    soma_dt += delta_time
+
+    if soma_dt > 1.0/30: # Aprox 30 quadros por segundo
+        soma_dt = 0
         
-        for f in self.faces:            
-            glBegin(GL_LINE_LOOP)
-            for iv in f:
-                v = self.vertices[iv]
-                glVertex(v.x, v.y, v.z)
-            glEnd()
+        if movimentos ==  "não":
+            o.MovimentoSim()
+            if o.estados == "finalizado":
+                movimentos = "trasFrenteConcluido"
+        elif movimentos == "trasFrenteConcluido":
+            o.QuedaCabeca()
+            if o.estados == "finalizado":
+                movimentos = "quedaConcluido"
+        elif movimentos == "quedaConcluido":
+            # movimento tornado
+            movimentos = "tornadoConcluido"
         
-        glPopMatrix()
-        pass
-
-    def Desenha(self):
-        glPushMatrix()
-        glTranslatef(self.position.x, self.position.y, self.position.z)
-        glRotatef(self.rotation[3], self.rotation[0], self.rotation[1], self.rotation[2])
-        glColor3f(0.34, .34, .34)
-        glLineWidth(2)        
+        elif movimentos == "tornadoConcluído":
+            # retornar cabeça
+            movimentos = "cabecaReformada"
+        elif movimentos == "cabecaReformada":
+            #novo movimento
+            movimentos = "cabou"        
         
-        for f in self.faces:            
-            glBegin(GL_TRIANGLE_FAN)
-            for iv in f:
-                v = self.vertices[iv]
-                glVertex(v.x, v.y, v.z)
-            glEnd()
-        
-        glPopMatrix()
-        pass
+        glutPostRedisplay()
 
 
+def desenha():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # Movimento de "sim"
-    def MovimentoSim(self):
-        self.fase += 0.1
-        angulo = self.amplitude * math.sin(self.fase)
-        for i in range(len(self.vertices)):
-            v = self.vertices[i]
-            y = v.y - self.centro.y
-            z = v.z - self.centro.z
-               
-            v.y = math.cos(angulo) * y - math.sin(angulo) * z + self.centro.y
-            v.z = math.sin(angulo) * y + math.cos(angulo) * z + self.centro.z
+    glMatrixMode(GL_MODELVIEW)
 
-    # Estrutura que faz os pontos descerem 
-    def QuedaCabeca(self):
+    DesenhaPiso()
+    #DesenhaCubo()    
+
+    # classes que desenham as faces
+    #o.Desenha() 
+    #o.DesenhaWireframe()
+
+    o.DesenhaVertices()
+
+    glutSwapBuffers()
+    pass
+
+def teclado(key, x, y):
+    o.rotation = (1, 0, 0, o.rotation[3] + 2)    
+
+    glutPostRedisplay()
+    pass
+
+def main():
+
+    glutInit(sys.argv)
+
+    # Define o modelo de operacao da GLUT
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH)
+
+    # Especifica o tamnho inicial em pixels da janela GLUT
+    glutInitWindowSize(640, 480)
+
+    # Especifica a posição de início da janela
+    glutInitWindowPosition(400, 400)
+
+    # Cria a janela passando o título da mesma como argumento
+    glutCreateWindow(b'Computacao Grafica - 3D')
+
+    # Função responsável por fazer as inicializações
+    init()
     
-     for i in range(len(self.vertices)):
-            self.angle[i] += self.speed[i] * (1/30)
 
-            x = self.radius[i] * math.cos(self.angle[i])
-            z = self.radius[i] * math.sin(self.angle[i])
+    # Registra a funcao callback de redesenho da janela de visualizacao
+    glutDisplayFunc(desenha)
 
-            self.vertices[i].x = x
-            self.vertices[i].z = z
+    # Registra a funcao callback para tratamento das teclas ASCII
+    glutKeyboardFunc(teclado)
+    
+    # Botões
+    btn_pausa = QPushButton("Pausar")
+    btn_voltas = QPushButton("Pausar")
+    btn_ir = QPushButton("Adiantar")
+    btn_girar = QPushButton("Girar")
 
-            if self.vertices[i].y >= 0.25:
-                self.vertices[i].y -= 0.25
-     pass
-                     
-    def Quicar(self):
-     
-     # Ir sempre até a metade da altura e atualizar até que ela seja igual a 0
-     for i in range(len(self.vertices)):
-        #if self.vertices[i].y <= 0.25:
-            self.angle[i] += self.speed[i] * (1/30)
-            novaAltura = self.vertices[i].y /2
-            self.vertices[i].y = novaAltura
+
+    glutIdleFunc(Animacao)
+
+    try:
+        # Inicia o processamento e aguarda interacoes do usuario
+        glutMainLoop()
+    except SystemExit:
+        pass
+
+if __name__ == '__main__':
+    main()
