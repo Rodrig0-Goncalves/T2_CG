083ccb096c59cc4ac15c47c243500d21f963dded
Rodrigo Gonçalves <161329845+Rodrig0-Goncalves@users.noreply.github.com>
2025-06-15 10:01:04 -0300

    Concluído movimento de queda + quicar

--- Objeto3D.py
+++ Objeto3D.py
@@ -19,7 +19,11 @@ class Objeto3D:
         self.amplitude = 0.025
         self.estadoTrasFrente = ""
         self.estadoQueda = ""
+        self.estadoQuicar = ""
+        self.estadoTornado = ""
         self.verticesOriginais= []
+        self.alturaVertices = []
+        self.direcaoVertice = []
         
         pass
 
@@ -40,7 +44,6 @@ class Objeto3D:
                 
                 self.vertices.append(vertices)
                 self.verticesOriginais.append(Ponto(vertices.x, vertices.y, vertices.z))
-                
 
                 self.angle.append(math.atan2(float(values[3]), float(values[1])))
                 self.radius.append(math.hypot(float(values[1]), float(values[3])))
@@ -54,7 +57,11 @@ class Objeto3D:
                     # dividimos cada elemento por '/'
                     self.faces[-1].append(int(fInfo[0]) - 1) # primeiro elemento é índice do vértice da face
                     # ignoramos textura e normal
-                
+                    
+            for i in self.vertices: # para pegar a altura de cada vértice e as direções dele
+                self.alturaVertices.append(i.y/2)
+                self.direcaoVertice.append(-1)   
+                                    
         # calcular o centro da cabeça
         soma_x, soma_y, soma_z = 0, 0, 0
         contador = 0
@@ -138,38 +145,47 @@ class Objeto3D:
         if  self.fase > 2*math.pi and -0.01 < angulo < 0.01:
             self.estadoTrasFrente = "finalizado"
             self.fase  = 0
+            print("Finalizou movimento tras frente")
+            
     
 
     # Estrutura que faz os pontos descerem 
-    def QuedaCabeca(self):
-     finalizou = True
     
-     for i in range(len(self.vertices)):
-            self.angle[i] += self.speed[i] * (1/30)
+    def QuedaCabeca(self):
+        finalizou = True
+        # Ir sempre até a metade da altura e atualizar até que ela seja igual a 0
+        for i in range(len(self.vertices)):
+            verticeAtual = self.vertices[i]
+            direcao = self.direcaoVertice[i]
+            altura = self.alturaVertices[i] 
+                    
+            if altura <= 0.1:
+                verticeAtual.y = 0
+                continue
+            
+            finalizou = False
+            
+            if direcao == -1: #significa que ele esta descendo
+                verticeAtual.y -= 0.2
+                if verticeAtual.y <= 0:
+                    verticeAtual.y = 0
+                    self.direcaoVertice[i] = 1 # sobe os vértices
+            
+            elif direcao == 1:
+                verticeAtual.y += 0.2
+                if verticeAtual.y >= altura:
+                    self.alturaVertices[i] = altura/2
+                    if self.alturaVertices[i] <= 0.01:
+                        self.direcaoVertice[i] = 0
+                    else:
+                        self.direcaoVertice[i] = -1
+                    
+        if finalizou:
+            self.estadoQuicar = "finalizou"
+            
+        
 
-            x = self.radius[i] * math.cos(self.angle[i])
-            z = self.radius[i] * math.sin(self.angle[i])
-
-            self.vertices[i].x = x
-            self.vertices[i].z = z
-
-            if self.vertices[i].y > 0:
-                self.vertices[i].y -= 0.25
-            finalizou = False      
-                           
-            if finalizou:
-                self.estadoQueda = "finalizado"
-                     
-    def Quicar(self):
-     
-     # Ir sempre até a metade da altura e atualizar até que ela seja igual a 0
-     for i in range(len(self.vertices)):
-        #if self.vertices[i].y <= 0.25:
-            self.angle[i] += self.speed[i] * (1/30)
-            novaAltura = self.vertices[i].y /2
-            self.vertices[i].y = novaAltura
-
-    # Quarda os vértices inicias para usar quando resetar em "b"
+    # Quarda os vértices inicias para usar quando resetar em "b" - porém resetando programa desde o ínicio, ajustar
     def resetarVertices(self):
         for i in range(len(self.vertices)):
             self.vertices[i].x = self.verticesOriginais[i].x
@@ -180,3 +196,4 @@ class Objeto3D:
             self.estadoQueda = ""
             self.estadoTrasFrente = ""
         
+         #self.angle[i] += self.speed[i] * (1/30) faz os vértices girarem 
